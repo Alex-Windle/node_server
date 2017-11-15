d81cdf2ccf05c82bdf81a2160cc196385709b91b
Alex Windle <Alex-Windle@users.noreply.github.com>
2017-11-15 09:44:46 -0500

    changed greeting

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express(); //Generates an Express application object.
 //appropriate route handlers. 
 
 app.get('/', (req, res) => {
-	res.send({ hi: 'there' });
+	res.send({ bye: 'buddy' });
 });
 //GET is a route handler (method on application object)
 //The first argument '/' specifies the route (url)
@@ -15,15 +15,15 @@ app.get('/', (req, res) => {
 
 //Deploy application with Heroku 
 //  Checklist: 
-//  (1) Specify port 
-//  (2) Specify mode environment 
-//  (3) Start Script
-//  (4) Gitignore file
-//Heroku specifies the port
-//Specify node version, start script (package.json)
-//Create .gitignore file (do not commit dependencies,
-//Heroku will install dependencies). Ignore node_modules folder.
-
+//  (1) Port (Heroku specifies)
+//  (2) Specify mode environment (package.json)
+//  (3) Start Script (package.json)
+//  (4) Gitignore file (do not commit dependencies,
+//  Heroku will install dependencies). Ignore node_modules folder.
+//Deploy via CLI with "heroku login" and "heroku create" commands.
+//The url and remote repository print: 
+//https://peaceful-chamber-78967.herokuapp.com/ | https://git.heroku.com/peaceful-chamber-78967.git
+//View deployed app with "heroku open" or point browser to url 
 const PORT = process.env.PORT || 5000;
 //If there is an environment variable defined by Heroku,
 //then use that port (prod). Or, use 5000 (dev). 
